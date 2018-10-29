3d283454eff4a0f4fefddad2f2fd5245a33cb00b
gabrieldecastilho <44000769+gabrieldecastilho@users.noreply.github.com>
2018-10-29 21:27:09 +0100

    Funktion rotateCard in CardLabel.java
    Karten drehen sich bei Refresh von PlayerPane

--- src/poker/version_graphics/view/CardLabel.java
+++ src/poker/version_graphics/view/CardLabel.java
@@ -1,8 +1,12 @@
 package poker.version_graphics.view;
 
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
+import javafx.util.Duration;
 import poker.version_graphics.model.Card;
 
 public class CardLabel extends Label {
@@ -20,9 +24,20 @@ public class CardLabel extends Label {
 			imv.fitHeightProperty().bind(this.heightProperty());
 			imv.setPreserveRatio(true);
 			this.setGraphic(imv);
+			rotateCard(imv,card);
 		} else {
 			this.setGraphic(null);
 		}
+	}
+	
+	private void rotateCard(ImageView imv, Card card) {
+		RotateTransition rotator = new RotateTransition(Duration.millis(1000), imv);
+        rotator.setAxis(Rotate.Y_AXIS);
+        rotator.setFromAngle(0);
+        rotator.setToAngle(360);
+        rotator.setInterpolator(Interpolator.LINEAR);
+        rotator.setCycleCount(1);
+        rotator.play();
 	}
 
 	private String cardToFileName(Card card) {
