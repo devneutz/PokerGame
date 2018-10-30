f12187797bb6e4a8e48cb245c95408c42d9360fa
Joel N <joel.neutzner@hotmail.com>
2018-10-30 17:12:42 +0100

    - tiebreaker fixed

--- src/poker/version_graphics/model/HandType.java
+++ src/poker/version_graphics/model/HandType.java
@@ -161,7 +161,7 @@ public enum HandType {
         return found;        
     }
     
-    protected int compareTieBreaker(ArrayList<Card> inCardsOne, ArrayList<Card> inCardsTwo) {
+    public int compareTieBreaker(ArrayList<Card> inCardsOne, ArrayList<Card> inCardsTwo) {
     	// Since the compareTo method on Enums is final, we do the tie break comparison here. another solution would be writing a 
     	// custom Comparator<HandType>
     	Collections.sort(inCardsOne, new Comparator<Card>() {
